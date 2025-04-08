70bbd7fd3cd5839e0e1ca2a4a888b378a759dce6
Colorful <colorfulmulberry@gmail.com>
2025-04-08 14:46:31 -0400

    Question class implementation

--- backend/LanguageLearner/question/models.py
+++ backend/LanguageLearner/question/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Question(models.Model):
+    question = models.CharField(max_length=100)
+    prompt = models.CharField(max_length=100)
+    answer = models.CharField(max_length=50)
+    choices = models.TextField()
+
+    # determines what the string version of the object returns
+    # must be indented to be a part of the class
+    def __str__(self):
+        return self.question
--- backend/LanguageLearner/question/views.py
+++ backend/LanguageLearner/question/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def question_list(request):
+    return render(request, "question/question_list.html")
