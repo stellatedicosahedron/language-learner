8679678e45bf04497ec79b54a5234c0432668b83
Colorful <colorfulmulberry@gmail.com>
2025-04-09 16:25:32 -0400

    added foreign key field to link each question to its quiz

--- backend/LanguageLearner/question/models.py
+++ backend/LanguageLearner/question/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from quiz.models import Quiz
 
 
 # Question model
@@ -6,7 +7,9 @@ class Question(models.Model):
     question = models.CharField(max_length=100)
     prompt = models.CharField(max_length=100)
     answer = models.CharField(max_length=50)
+    # add a default json object here to prevent errors
     choices = models.TextField()
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, default=None)
 
     # determines what the string version of the object returns
     # must be indented to be a part of the class
